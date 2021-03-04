d448b0839238350cf84f68e6487bba69930a0b1b
latifbaraba <latifbaraba88@gmail.com>
2021-03-04 23:55:40 +0700

    kontak

--- src/components/Common/Contact.jsx
+++ src/components/Common/Contact.jsx
@@ -102,131 +102,38 @@ class Contact extends React.Component {
             <section id="contact" className="contact-area ptb-80 bg-f6f6f6">
                 <div className="container">
                     <div className="section-title">
-                        <h4>Get in Touch</h4>
-                        <h2>Let's <span>Contact</span> Us</h2>
+                        {/* <h4>Get in Touch</h4> */}
+                        <h2>Kontak <span>kami</span></h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
                     
                     <div className="row">
-                        <div className="col-lg-6 col-md-12">
-                            <div id="map">
-                                <div style={{ height: '100%', width: '100%' }}>
-                                    <GoogleMapReact
-                                        bootstrapURLKeys={{ key: 'AIzaSyDTWAxQ7Z4GNC28ljvgSDBAmTiZ-eYiGog' }}
-                                        defaultCenter={this.props.center}
-                                        defaultZoom={this.props.zoom}
-                                    >
-                                        <CustomComponent
-                                            lat={59.955413}
-                                            lng={30.337844}
-                                            text="Marker"
-                                        />
-                                    </GoogleMapReact>
+                        
+                        <div className="col-lg-4 col-md-12">
+                            {/* <div className="contact-form">
+                                <h4>Stay Connected</h4>
+                            </div> */}
+                            <div className="col-lg-12">
+                                <div className="contact-one">
+                                    <h4>Stay Connected</h4>
                                 </div>
                             </div>
-                            
-                            <div className="contact-info">
-                                <ul>
-                                    <li>
-                                        <i className="fa fa-map-marker"></i> Exercisplan 4, 111 49 Stockholm, Sweden
-                                    </li>
-                                    <li>
-                                        <i className="fa fa-envelope"></i> 
-                                        <Link to="#">addax@gmail.com</Link>
-                                    </li>
-                                    <li>
-                                        <i className="fa fa-phone"></i> 
-                                        <Link to="#">(+124)412-2445515</Link>
-                                    </li>
-                                    <li>
-                                        <i className="fa fa-fax"></i> 
-                                        <Link to="#">617-241-60055</Link>
-                                    </li>
-                                </ul>
+                            <div className="col-lg-12">
+                                <div className="contact-one">
+                                    <h4>Stay Connected</h4>
+                                </div>
                             </div>
                         </div>
-                        
-                        <div className="col-lg-6 col-md-12">
-                            <div className="contact-form">
+
+                        <div className="col-lg-4 col-md-12">
+                            <div className="contact-two">
                                 <h4>Stay Connected</h4>
-                                <form id="contactForm" onSubmit={this.onSubmit}>
-                                    <div className="row">
-                                        <div className="col-lg-12 col-md-6">
-                                            <div className="form-group">
-                                                <label htmlFor="name">Name</label>
-                                                <input 
-                                                    type="text" 
-                                                    className="form-control" 
-                                                    name="name" 
-                                                    id="name" 
-                                                    required={true}
-                                                    data-error="Please enter your name" 
-                                                    value={this.state.formFields.name}
-                                                    onChange={this.nameChangeHandler}
-                                                />
-                                                <div className="help-block with-errors"></div>
-                                            </div>
-                                        </div>
+                            </div>
+                        </div>
 
-                                        <div className="col-lg-12 col-md-6">
-                                            <div className="form-group">
-                                                <label htmlFor="email">Email</label>
-                                                <input 
-                                                    type="email" 
-                                                    className="form-control" 
-                                                    name="email" 
-                                                    id="email" 
-                                                    required={true}
-                                                    data-error="Please enter your email" 
-                                                    value={this.state.formFields.email}
-                                                    onChange={this.emailChangeHandler}
-                                                />
-                                                <div className="help-block with-errors"></div>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-lg-12 col-md-12">
-                                            <div className="form-group">
-                                                <label htmlFor="number">Phone Number</label>
-                                                <input 
-                                                    type="text" 
-                                                    className="form-control" 
-                                                    name="number" 
-                                                    id="number" 
-                                                    required={true}
-                                                    data-error="Please enter your number" 
-                                                    value={this.state.formFields.phone}
-                                                    onChange={this.phoneChangeHandler}
-                                                />
-                                                <div className="help-block with-errors"></div>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-lg-12 col-md-12">
-                                            <div className="form-group">
-                                                <label htmlFor="message">Message</label>
-                                                <textarea 
-                                                    name="message" 
-                                                    className="form-control" 
-                                                    id="message" 
-                                                    cols="30" 
-                                                    rows="4" 
-                                                    required={true}
-                                                    data-error="Write your message" 
-                                                    value={this.state.formFields.text}
-                                                    onChange={this.textChangeHandler}
-                                                />
-                                                <div className="help-block with-errors"></div>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-lg-12 col-md-12">
-                                            <button type="submit" className="btn btn-primary">Send Message</button>
-                                            {this.successMessage()}
-                                            <div className="clearfix"></div>
-                                        </div>
-                                    </div>
-                                </form>
+                        <div className="col-lg-4 col-md-12">
+                            <div className="contact-two">
+                                <h4>Stay Connected</h4>
                             </div>
                         </div>
                     </div>
